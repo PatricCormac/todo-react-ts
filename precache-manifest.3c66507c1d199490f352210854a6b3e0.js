5ded801ef74d0482c88e1c79610e50fe39ec8362
PatricCormac <socolov1897@gmail.com>
2020-08-29 03:48:40 +0500

    Updates

---
+++ precache-manifest.3c66507c1d199490f352210854a6b3e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bf19acab021f5fbe123d881e391e03",
+    "url": "/todo-react-ts/index.html"
+  },
+  {
+    "revision": "7be4b050a2797e85a0e1",
+    "url": "/todo-react-ts/static/css/main.853f9790.chunk.css"
+  },
+  {
+    "revision": "8b39b08b2907e083e2f5",
+    "url": "/todo-react-ts/static/js/2.6f122f55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react-ts/static/js/2.6f122f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be4b050a2797e85a0e1",
+    "url": "/todo-react-ts/static/js/main.e6788cd1.chunk.js"
+  },
+  {
+    "revision": "ef39a1be74c761457387",
+    "url": "/todo-react-ts/static/js/runtime-main.3e9c5c88.js"
+  }
+]);
