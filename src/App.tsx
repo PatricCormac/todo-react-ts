daf85ca52433148581ee144a207612901db99328
PatricCormac <socolov1897@gmail.com>
2020-08-30 02:39:48 +0500

    Добавил localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,15 @@ import { ITodo } from './interfaces'
 const App: React.FC = () => {
   const [todos, setTodos] = useState<ITodo[]>([])
 
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
+    setTodos(saved)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
   const addHandler = (title: string) => {
     const newTodo: ITodo = {
       title,
